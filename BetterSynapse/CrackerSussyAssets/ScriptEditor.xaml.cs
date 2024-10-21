95e21056c95d1c3272b5f0098f275f16542a8eea
unknown <Admin@372210653-1930121430-2398921267.372210653-1930121430-2398921267>
2024-10-20 22:36:41 -0500

    Initial Commit

---
+++ BetterSynapse/CrackerSussyAssets/ScriptEditor.xaml.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+using System.Xml;
+using CefSharp;
+using CefSharp.Wpf;
+using ICSharpCode.AvalonEdit;
+using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using ICSharpCode.AvalonEdit.Rendering;
+using Newtonsoft.Json;
+using SynapseX.Properties;
+
+namespace SynapseX.CrackerSussyAssets
+{
+    /// <summary>
+    /// Interaction logic for ScriptEditor.xaml
+    /// </summary>
+    public static class Extensions
+    {
+        public static string EvaluateScript(this IWebBrowser browser, string script)
+        {
+            var value = browser.EvaluateScriptAsync(script);
+            value.Wait();
+            return value.Result.Success ? value.Result.Result.ToString() : "";
+        }
+    }
+    
+    public partial class ScriptEditor : UserControl
+    {
+        private string TabDir;
+        public ChromiumWebBrowser SelectedEditor;
+
+        public ScriptEditor()
+        {
+            InitializeComponent();
+        }
+
+        private static TextEditor CreateNewEditor(string content)
+        {
+            var editor = new TextEditor
+            {
+                Background = Brushes.Transparent,
+                Margin = new Thickness(1),
+                HorizontalScrollBarVisibility = ScrollBarVisibility.Disabled,
+                VerticalScrollBarVisibility = ScrollBarVisibility.Visible,
+                ShowLineNumbers = true,
+                FontFamily = new FontFamily(Settings.Default.FontName),
+                FontSize = Settings.Default.FontSize,
+                Text = content
+            };
+            
+            editor.TextArea.TextView.SetResourceReference(TextView.LinkTextForegroundBrushProperty, "PrimaryTextColor");
+            editor.SetResourceReference(TextEditor.ForegroundProperty, "PrimaryTextColor");
+            editor.SetResourceReference(TextEditor.LineNumbersForegroundProperty, "PrimaryTextColor");
+
+            XmlReader xml = new XmlTextReader(File.OpenRead("bs_bin/syntax.xhsd"));
+            editor.SyntaxHighlighting = HighlightingLoader.Load(xml, HighlightingManager.Instance);
+
+            editor.TextChanged += delegate
+            {
+                var offset = editor.CaretOffset;
+                var line = editor.Document.GetLineByOffset(offset);
+                editor.ScrollToLine(line.LineNumber);
+            };
+
+            Settings.Default.PropertyChanged += (_, args) =>
+            {
+                switch (args.PropertyName)
+                {
+                    case "FontName":
+                        editor.FontFamily = new FontFamily(Settings.Default.FontName);
+                        break;
+
+                    case "FontSize":
+                        editor.FontSize = Settings.Default.FontSize;
+                        break;
+                }
+            };
+
+            return editor;
+        }
+
+        private static ChromiumWebBrowser CreateNewMonacoEditor()
+        {
+            var cefSettings = new CefSettings
+            {
+                BrowserSubprocessPath = Path.GetFullPath("bs_bin/CefSharp.BrowserSubprocess.exe"),
+                LocalesDirPath = Path.GetFullPath("bs_bin/locales"),
+                ResourcesDirPath = Path.GetFullPath("bs_bin"),
+                LogFile = Path.GetFullPath("bs_bin/cefsharp.log")
+            };
+            
+            var editor = new ChromiumWebBrowser
+            {
+                Address = $"file:///{Directory.GetCurrentDirectory()}/bs_bin/editor_files/rosploco.html"
+            };
+
+            return editor;
+        }
+
+        public void AddTab(string file)
+        {
+            var info = new FileInfo(file);
+            var content = File.ReadAllText(file);
+
+            var editor = CreateNewMonacoEditor();
+            editor.Visibility = Visibility.Hidden;
+
+            var item = new TabItem
+            {
+                Header = info.Name,
+                Content = editor,
+                Tag = file
+            };
+
+            var panel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal,
+                Focusable = false
+            };
+
+            var text = new TextBlock
+            {
+                Text = info.Name,
+                Focusable = true,
+            };
+
+            var removeButton = new Button
+            {
+                Content = "\xE8BB",
+                FontSize = 8,
+                Width = 12,
+                Height = 12,
+                Margin = new Thickness(5, 0, 0, 0),
+                Visibility = Visibility.Collapsed,
+                FontFamily = new FontFamily("Segoe MDL2 Assets"),
+                Background = null
+            };
+
+            text.SetResourceReference(TextBlock.ForegroundProperty, "PrimaryTextColor");
+            removeButton.SetResourceReference(Button.ForegroundProperty, "PrimaryTextColor");
+
+            panel.Children.Add(text);
+            panel.Children.Add(removeButton);
+
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            removeButton.Click += delegate
+            {
+                if (Tab.Items.Count == 1) return;
+                timer.IsEnabled = false;
+                RemoveTab(item);
+            };
+
+            item.Header = panel;
+            item.Height = 24;
+
+            Tab.Items.Add(item);
+
+            timer.Tick += delegate
+            {
+                Save(item);
+            };
+
+            editor.LoadingStateChanged += (sender, args) =>
+            {
+                Dispatcher.Invoke(async () =>
+                {
+                    if (args.IsLoading) return;
+                    if (!File.Exists(file)) return;
+
+                    await editor.EvaluateScriptAsync("setText", File.ReadAllText(file));
+                    await Task.Delay(500);
+                    editor.Visibility = Visibility.Visible;
+                    timer.IsEnabled = !args.IsLoading;
+                });
+            };
+
+            Tab.SelectionChanged += delegate
+            {
+                removeButton.Visibility = item.IsSelected ? Visibility.Visible : Visibility.Collapsed;
+                if (item.IsSelected)
+                {
+                    SelectedEditor = editor;
+                    CurrentFileName = text.Text;
+                }
+
+                else Save(item);
+                TabChanged?.Invoke(item, EventArgs.Empty);
+            };
+        }
+
+        public void CreateTab(string fileName, string content = "-- Monaco and Autocomplete by Microsoft and EthanMcBloxxer on GitHub under the MIT License.")
+        {
+            fileName = Path.Combine(TabDir, fileName);
+
+            if (File.Exists(fileName))
+            {
+                Messages.ShowGenericErrorMessage("File already exists!");
+                return;
+            }
+            File.WriteAllText(fileName, content);
+            AddTab(fileName);
+        }
+
+        public void RemoveTab(TabItem item)
+        {
+            var file = (string)item.Tag;
+            if (File.Exists(file))
+                File.Delete(file);
+
+            Tab.Items.Remove(item);
+        }
+
+        //public void EditTab(TabItem item, string name)
+        //{
+        //    var oldFileName = (string)item.Tag;
+
+        //    if (File.Exists(oldFileName))
+        //    {
+        //        File.Move(oldFileName, name);
+        //        item.Tag = name;
+        //    }
+
+        //    else
+        //        Tab.Items.Remove(item);
+        //}
+
+        public void RefreshAllTabs(bool SaveAll = true)
+        {
+            if (SaveAll) SaveAllTabs();
+            var files = Directory.GetFiles(TabDir);
+            if (files.Length == 0)
+            {
+                File.WriteAllText(Path.Combine(TabDir, "Script 0.lua"), "");
+                files = Directory.GetFiles(TabDir);
+            }
+
+            Tab.Items.Clear();
+            foreach (var file in files)
+                AddTab(file);
+            Tab.SelectedIndex = 0;
+        }
+
+        public void Save(TabItem item)
+        {
+            var file = (string)item.Tag;
+            if (!File.Exists(file)) return;
+
+            var editor = (ChromiumWebBrowser)item.Content;
+            if (!editor.CanExecuteJavascriptInMainFrame) return;
+
+            var script = editor.EvaluateScript("getText()");
+            File.WriteAllText(file, script);
+            Debug.WriteLine($"Saved {Path.GetFileName(item.Tag.ToString())} | {script}");
+        }
+
+        public void SaveAllTabs()
+        {
+            foreach (TabItem item in Tab.Items)
+                Save(item);
+        }
+        
+        public event EventHandler TabChanged;
+        public string CurrentFileName = "";
+
+        public void InitializeDirectory(string dir)
+        {
+            TabDir = dir;
+            RefreshAllTabs(false);
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            var count = 0;
+            var name = $"Script {count}.lua";
+            while (File.Exists(Path.Combine(TabDir, name)))
+            {
+                count++;
+                name = $"Script {count}.lua";
+            }
+
+            CreateTab(name);
+        }
+    }
+
+}
---
+++ BetterSynapse/MainWindow.xaml.cs
@@ -0,0 +1,352 @@
+﻿// the unmodified source is from https://github.com/rice-cracker-2234/BetterSynapse/
+// this is ported to support seliware by https://github.com/weekndds (it was extremely easy cause we love seliware api)
+
+using CefSharp;
+using CefSharp.Wpf;
+using Newtonsoft.Json;
+using SeliwareAPI;
+using SynapseX.CrackerSussyAssets;
+using SynapseX.Properties;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using Brushes = System.Windows.Media.Brushes;
+using ContextMenu = System.Windows.Controls.ContextMenu;
+using FontFamily = System.Windows.Media.FontFamily;
+using MenuItem = System.Windows.Controls.MenuItem;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+using Orientation = System.Windows.Controls.Orientation;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
+
+namespace SynapseX
+{
+    public partial class MainWindow : Window
+    {
+        private ManagementEventWatcher processStartWatcher;
+        private const string processName = "RobloxPlayerBeta.exe";
+        public MainWindow()
+        {
+            Settings.Default.PropertyChanged += (_, _) => Settings.Default.Save();
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) => File.AppendAllText("bs_bin/error.log", args.ExceptionObject + Environment.NewLine + Environment.NewLine);
+
+            InitializeComponent();
+            Seliware.Initialize();
+
+            Seliware.Injected += OnSeliwareInjected;
+            LoadSettings();
+            SetupProcessWatcher();
+        }
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            Directory.CreateDirectory("scripts");
+            Directory.CreateDirectory("bs_bin/tabs");
+            Editor.InitializeDirectory("bs_bin/tabs");
+
+            await PopulateScriptTree(ScriptList, "scripts");
+            var watcher = new FileSystemWatcher("scripts")
+            {
+                NotifyFilter = NotifyFilters.Attributes
+                               | NotifyFilters.CreationTime
+                               | NotifyFilters.DirectoryName
+                               | NotifyFilters.FileName
+                               | NotifyFilters.LastAccess
+                               | NotifyFilters.LastWrite
+                               | NotifyFilters.Security
+                               | NotifyFilters.Size
+            };
+
+
+            async void OnFileEvent(object obj, FileSystemEventArgs eventArgs)
+            {
+                await Dispatcher.InvokeAsync(async delegate
+                {
+                    await PopulateScriptTree(ScriptList, "scripts");
+                });
+            }
+
+            watcher.Changed += OnFileEvent;
+            watcher.Created += OnFileEvent;
+            watcher.Deleted += OnFileEvent;
+            watcher.Renamed += OnFileEvent;
+
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+        }
+
+
+        private void DraggableBorder_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
+        private void StateButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            StateButton.Content = WindowState == WindowState.Maximized ? '\xe923' : '\xe922';
+            Main.Margin = WindowState == WindowState.Maximized ? new Thickness(7) : new Thickness(0);
+        }
+
+        private async Task PopulateScriptTree(ItemsControl treeView, string directory)
+        {
+            // this code is pretty bad but it'll work for now.
+            // ^^ this comment was NOT made by me - wish
+            treeView.Items.Clear();
+
+            foreach (var path in Directory.GetDirectories(directory))
+            {
+                var info = new DirectoryInfo(path);
+
+                void OnDeleteDirectoryClick(object sender, RoutedEventArgs e)
+                {
+                    if (Messages.ShowGenericWarningMessage())
+                        Directory.Delete(path, true);
+                }
+
+                var panel = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(2) };
+                var icon = new TextBlock
+                {
+                    FontFamily = TryFindResource("Segoe Fluent Icons") as FontFamily,
+                    FontSize = 14,
+                    Text = "\xe8b7",
+                    Margin = new Thickness(0, 0, 3, 0)
+                };
+
+                var content = new TextBlock
+                {
+                    FontSize = 12,
+                    Text = info.Name,
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+
+                panel.Children.Add(icon);
+                panel.Children.Add(content);
+
+                var item = new TreeViewItem { Header = panel };
+
+                var menu = new ContextMenu();
+
+                var deleteDirectory = new MenuItem { Header = "Delete Directory" };
+                deleteDirectory.Click += OnDeleteDirectoryClick;
+
+                menu.Items.Add(deleteDirectory);
+                item.ContextMenu = menu;
+                treeView.Items.Add(item);
+                await PopulateScriptTree(item, path);
+            }
+
+            foreach (var file in Directory.GetFiles(directory))
+            {
+                var info = new FileInfo(file);
+
+                void OnExecuteClick(object sender, RoutedEventArgs e)
+                {
+                    var script = File.ReadAllText(file);
+                    Seliware.Execute(script);
+                }
+
+                void OnLoadScriptClick(object sender, RoutedEventArgs e)
+                {
+                    if (Messages.ShowGenericQuestionMessage("Open in a new tab?"))
+                        Editor.CreateTab(info.Name, File.ReadAllText(file));
+                    else
+                        Editor.SelectedEditor.ExecuteScriptAsync($"setText({JsonConvert.SerializeObject(File.ReadAllText(file))})");
+                }
+
+                void OnSaveScriptClick(object sender, RoutedEventArgs e) => File.WriteAllText(file, Editor.SelectedEditor.EvaluateScript("getText()"));
+
+                void OnDeleteScriptClick(object sender, RoutedEventArgs e)
+                {
+                    if (Messages.ShowGenericWarningMessage())
+                        File.Delete(file);
+                }
+
+                var panel = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(2) };
+                var icon = new TextBlock
+                {
+                    FontFamily = TryFindResource("Codicon") as FontFamily,
+                    FontSize = 14,
+                    Text = "\xea7b",
+                    Margin = new Thickness(0, 0, 3, 0)
+                };
+
+                var content = new TextBlock
+                {
+                    FontSize = 12,
+                    Text = info.Name,
+                    VerticalAlignment = VerticalAlignment.Center
+                };
+
+                panel.Children.Add(icon);
+                panel.Children.Add(content);
+
+                var item = new TreeViewItem { Header = panel };
+
+                var menu = new ContextMenu();
+
+                var executeScript = new MenuItem { Header = "Execute Script" };
+                executeScript.Click += OnExecuteClick;
+
+                var loadScript = new MenuItem { Header = "Load Script" };
+                loadScript.Click += OnLoadScriptClick;
+
+                var saveScript = new MenuItem { Header = "Save Script" };
+                saveScript.Click += OnSaveScriptClick;
+
+                var deleteScript = new MenuItem { Header = "Delete Script" };
+                deleteScript.Click += OnDeleteScriptClick;
+
+                menu.Items.Add(executeScript);
+                menu.Items.Add(loadScript);
+                menu.Items.Add(saveScript);
+                menu.Items.Add(deleteScript);
+                item.ContextMenu = menu;
+                treeView.Items.Add(item);
+            }
+        }
+
+        public static readonly string FileFilter = FilterInstance.ToString(new[]
+        {
+            new FilterInstance
+            {
+                Title = "Text Document",
+                Filter = "*.txt",
+                IncludeFilter = true
+            },
+
+            new FilterInstance
+            {
+                Title = "LUA Script",
+                Filter = "*.lua",
+                IncludeFilter = true
+            }
+        });
+
+        private void Panels_Click(object sender, RoutedEventArgs e) => ((Storyboard)TryFindResource("PanelClosedStoryboard")).Begin();
+        private void SettingsButton_Click(object sender, RoutedEventArgs e) => ((Storyboard)TryFindResource("SettingsOpenStoryboard")).Begin();
+        private void ScriptHubButton_Click(object sender, RoutedEventArgs e) => ((Storyboard)TryFindResource("ScriptHubOpenStoryboard")).Begin();
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var saveDialog = new SaveFileDialog
+            {
+                Title = "Save File",
+                Filter = FileFilter,
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (saveDialog.ShowDialog() == true) File.WriteAllText(saveDialog.FileName, Editor.SelectedEditor.EvaluateScript("getText()"));
+        }
+
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openDialog = new OpenFileDialog
+            {
+                Title = "Open File",
+                Filter = FileFilter,
+                FilterIndex = 1,
+                RestoreDirectory = true
+            };
+
+            if (openDialog.ShowDialog() != true) return;
+            if (Messages.ShowGenericQuestionMessage("Open in a new tab?"))
+                Editor.CreateTab(Path.GetFileName(openDialog.FileName), File.ReadAllText(openDialog.FileName));
+            else Editor.SelectedEditor.ExecuteScriptAsync("setText", File.ReadAllText(openDialog.FileName));
+        }
+
+        private void LoadSettings()
+        {
+            TopMost.IsChecked = Topmost = Settings.Default.TopMost;
+            AutoAttach.IsChecked = Settings.Default.AutoAttach;
+            UnlockFPS.IsChecked = Settings.Default.UnlockFPS;
+        }
+
+        private void SetupProcessWatcher()
+        {
+            string startQuery = $"SELECT * FROM Win32_ProcessStartTrace WHERE ProcessName = '{processName}'";
+            processStartWatcher = new ManagementEventWatcher(startQuery);
+            processStartWatcher.EventArrived += OnRobloxProcessStarted;
+            processStartWatcher.Start();
+        }
+
+
+        private void OnRobloxProcessStarted(object sender, EventArrivedEventArgs e)
+        {
+            if (Settings.Default.AutoAttach)
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    Seliware.Inject();
+                });
+            }
+        }
+
+        private void OnSeliwareInjected(object sender, EventArgs e)
+        {
+            try
+            {
+                Dispatcher.Invoke(() =>
+                {
+                    InjectState.Content = "Attached";
+                    StateColor.Fill = Brushes.Lime;
+
+                    //  for some reason seliware's setfpscap doesnt work so
+                    //if (Settings.Default.UnlockFPS)
+                    //{
+                    //    Seliware.Execute("setfpscap(99999)");
+                    //}
+                });
+            }
+            catch (Exception ex)
+            {
+                File.AppendAllText("bs_bin/error.log", $"Error in OnSeliwareInjected: {ex}" + Environment.NewLine);
+            }
+        }
+
+        private void TopMost_Checked(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.TopMost = Topmost = TopMost.IsChecked.GetValueOrDefault();
+            Settings.Default.Save();
+        }
+
+        private void AutoAttach_Checked(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.AutoAttach = AutoAttach.IsChecked.GetValueOrDefault();
+            Settings.Default.Save();
+        }
+
+        private void UnlockFPS_Checked(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.UnlockFPS = UnlockFPS.IsChecked.GetValueOrDefault();
+            Settings.Default.Save();
+        }
+
+
+        private void AttachButton_Click(object sender, RoutedEventArgs e) => Seliware.Inject();
+        private void ExecuteButton_Click(object sender, RoutedEventArgs e) => Seliware.Execute(Editor.SelectedEditor.EvaluateScript("getText()"));
+
+        private void KillRobloxButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var process in Process.GetProcessesByName("RobloxPlayerBeta"))
+                process.Kill();
+        }
+
+        private void OnWindowClosed(object sender, EventArgs e)
+        {
+            processStartWatcher?.Stop();
+            processStartWatcher?.Dispose();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
